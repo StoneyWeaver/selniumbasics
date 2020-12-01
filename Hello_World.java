629aeac00e0522c12771fb0b89df74b547786746
StoneyWeaver <39928906+StoneyWeaver@users.noreply.github.com>
2020-12-01 14:52:16 -0600

    This is my first java project
    I use syso via direct print and also reference to a decalred string to print the basic hello world stagement as a string

---
+++ Hello_World.java
@@ -0,0 +1,20 @@
+package Intro_Package;
+
+public class Hello_World {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String print1 = "Hello World";
+
+System.out.println("Hello World");
+System.out.println(print1);
+
+//This is for a single line comment
+
+/*
+ * This is for
+ * multiple lines of comments
+ */
+	}
+
+}
